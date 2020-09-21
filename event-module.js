ab55c308d4c37ebcbeaccd61cbb75c2f824a78ab
sergiopoliveira <sergiopoliveira@outlook.com>
2020-09-21 19:59:38 +0100

    Event Argument

--- event-module.js
+++ event-module.js
@@ -2,9 +2,11 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 // Register a listener
-emitter.on('messageLogged',function(){
-  console.log('Listener called');
+emitter.on('messageLogged', (arg) => {
+  console.log('Listener called', arg);
 });
 
 // Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', { id: 1, url: 'http://'});
+
+// Raise: logging (data: message)
