5e8024d56cd21d69ea4082ed2b475f896f1e8f6f
sergiopoliveira <sergiopoliveira@outlook.com>
2020-09-21 18:33:24 +0100

    Loading a Module

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const log = require('./logger')
+
 function sayHello(name) {
     console.log('Hello ' + name);
 }
@@ -15,3 +17,7 @@ console.log(); // global object
 var message = ''; // only scoped to this file
 
 console.log(global.message); // undefined
+
+console.log(log);
+
+log('message');
