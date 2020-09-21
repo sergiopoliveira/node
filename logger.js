3162e36d6b4a07a5f94e1c8199cab4d2661bd226
sergiopoliveira <sergiopoliveira@outlook.com>
2020-09-21 20:09:54 +0100

    Extending EventEmitter

--- logger.js
+++ logger.js
@@ -1,14 +1,22 @@
+const EventEmitter = require('events');
+
 // Module Wrapper Function
 console.log(__filename);
 console.log(__dirname);
 var url = 'http://mylogger.io/log';
 
-function log(message) {
-    // Send an HTTP request
-    console.log(message);
+class Logger extends EventEmitter{
+
+    log(message) {
+        // Send an HTTP request
+        console.log(message);
+
+        // Raise an event
+        this.emit('messageLogged', { id: 1, url: 'http://' });
+    }
 }
 
 //module.exports.log = log;
 //module.exports.endPoint = url;
-module.exports = log;
+module.exports = Logger;
 
