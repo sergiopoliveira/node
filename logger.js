6bc2cc698a948c4b11f413318948c76ff2aec7d8
sergiopoliveira <sergiopoliveira@outlook.com>
2020-09-21 19:09:16 +0100

    Module Wrapper Function

--- logger.js
+++ logger.js
@@ -1,3 +1,6 @@
+// Module Wrapper Function
+console.log(__filename);
+console.log(__dirname);
 var url = 'http://mylogger.io/log';
 
 function log(message) {
@@ -8,3 +11,4 @@ function log(message) {
 //module.exports.log = log;
 //module.exports.endPoint = url;
 module.exports = log;
+
